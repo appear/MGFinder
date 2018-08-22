10c6b5ea802792a569e9116a6b4bc25684b25935
olaf <olaf@triple-corp.com>
2018-08-22 18:53:20 +0900

    fix: package 정리, 함수 내용 정리

--- libs/crawler.js
+++ libs/crawler.js
@@ -4,89 +4,82 @@ const cheerio = require('cheerio');
 const baseUrl = 'https://mvnrepository.com/search?q=';
 
 function run(query) {
-    return new Promise((resolve, reject) => {
-        request(baseUrl + query, (err, res, body) => {
-            if (err) throw err;
-            const $ = cheerio.load(body);
-            // 첫번째 a href 가져오기
-            const href = $($("#maincontent .im:nth-child(3) a:nth-child(2n)")[0]).attr('href');
-            console.log('run', href)
-            resolve(href);
-        });
-    })
+  return new Promise((resolve, reject) => {
+    request(baseUrl + query, (err, res, body) => {
+      if (err) throw err
+      const $ = cheerio.load(body)
+      // 첫번째 a href 가져오기
+      const href = $($("#maincontent .im:nth-child(3) a:nth-child(2n)")[0]).attr('href')
+      resolve(href)
+    });
+  })
 }
 
 function parse(href) {
-    let dataList = [];
-    const link = `https://mvnrepository.com/${href}`;
+  let dataList = []
+  const link = `https://mvnrepository.com/${href}`
 
-    return new Promise((resolve, reject) => {
-        request(link, (err, res, body) => {
-            if (err) throw err;
-            const $ = cheerio.load(body);
-            const trList = $(".gridcontainer tr");
+  return new Promise((resolve, reject) => {
+    request(link, (err, res, body) => {
+      if (err) throw err;
+      const $ = cheerio.load(body)
+      const trList = $(".gridcontainer tr")
 
-            // 테이블 구조때문에 한번더 거르는 작업이 필요
-            trList.each(function() {
-                if (!isNaN(convertUsages($(this)))) {
-                    dataList.push($(this));
-                }
-            });
-            resolve({ list: sortAndCut(10, dataList), link });
-        });
-    })
+      // 테이블 구조때문에 한번더 거르는 작업이 필요
+      trList.each(function () {
+        if (!isNaN(convertUsages($(this)))) {
+          dataList.push($(this))
+        }
+      });
+      const parseData = {list: sortAndCut(10, dataList), link}
+      printList(parseData)
+      resolve(parseData)
+    });
+  })
 }
 
 // 목록 보여주기
-function print({ list, link }) {
-    return new Promise((resolve, reject) => {
-        list.forEach((item, idx) => {
-            const title = item.find(".vbtn").text();
-            const usages = convertUsages(item);
-            console.log(`[${idx + 1}] ${title} : ${usages}`);
-        });
-        resolve({ list, link });
-    })
+function printList({list, link}) {
+  list.forEach((item, idx) => {
+    const title = item.find(".vbtn").text()
+    const usages = convertUsages(item)
+    console.log(`(${idx + 1}) ${title} : ${usages} 🙂`)
+  })
+  return {list, link}
 }
 
-function getUrl(obj, version) {
-    return new Promise((resolve, reject) => {
-        const title = obj.list[version - 1].find(".vbtn").text();
-        const link = `${obj.link}/${title}`;
-        resolve(link);
-    })
+function getSelectedUrl(obj, version) {
+  const title = obj.list[version - 1].find(".vbtn").text()
+  return `${obj.link}/${title}`
 }
 
 function result(link, type) {
-    return new Promise((resolve, reject) => {
-        request(link, (err, res, body) => {
-            if (err) throw err;
-            const $ = cheerio.load(body);
-            type === "1" ? printResult($, "#maven-a") : printResult($, "#gradle-a");
-            resolve(true);
-        })
+  return new Promise((resolve, reject) => {
+    request(link, (err, res, body) => {
+      if (err) throw err;
+      const $ = cheerio.load(body)
+      type === "1"
+        ? console.log($("#maven-a").text())
+        : console.log($("#gradle-a").text())
     })
+  })
 }
 
-// 결과값 출력
-function printResult($, selector) {
-    console.log($(selector).text());
-}
-
-// 사용이 많이된 순으로 10개 정렬
+// 사용이 많이된 순으로 10개 정렬 차후 count 갯수 조절 가능하도록 변경
 function sortAndCut(count, list) {
-    list.sort((a, b) => convertUsages(b) - convertUsages(a));
-    return list.slice(0, count);
+  return list.sort((a, b) =>
+    convertUsages(b) - convertUsages(a)
+  ).slice(0, count)
 }
 
 // Usages를 가져오고 , 를 없애기 위한 작업
 function convertUsages(el) {
-    return parseInt(el.find(`td:nth-child(${tdCount(el)}) a`).text().replace(/,/g, ""), 10);
+  return parseInt(el.find(`td:nth-child(${tdType(el)}) a`).text().replace(/,/g, ""), 10);
 }
 
 // 테이블 구조때문에... td의 갯수에 따라서 가져와야되는 것이 다름 ..
-function tdCount(el) {
-    return el.find("td").length === 5 ? 4 : 3;
+function tdType(el) {
+  return el.find("td").length === 5 ? 4 : 3;
 }
 
-module.exports = { run, parse, getUrl, print, result };
+module.exports = {run, parse, getSelectedUrl, result};
--- libs/finder.js
+++ libs/finder.js
@@ -1,39 +1,41 @@
 const ora = require('ora')
 const prompt = require('prompts')
-const { run, print, parse, getUrl, result } = require('./crawler')
+const { run, parse, getSelectedUrl, result } = require('./crawler')
 
-module.exports = async(flags) => {
+const prompts = [
+  {
+    type: 'text',
+    name: 'name',
+    message: 'What is your find lib name ?'
+  },
+  {
+    type: 'text',
+    name: 'version',
+    message: 'Please select a version (ex. 1)'
+  },
+  {
+    type: 'text',
+    name: 'category',
+    message: '1. Maven / 2. Gradle'
+  }
+]
+
+module.exports = async() => {
     const spinner = ora()
     spinner.color = 'yellow'
 
-    const libray = await prompt({
-        type: 'text',
-        name: 'name',
-        message: '라이브러리 이름이 뭐에여?'
-    });
+    const { name } = await prompt(prompts[0]);
 
-    spinner.text = 'Searching ..... :) '
+    spinner.text = 'Searching ..... 😃'
     spinner.start()
 
-    const href = await run(libray.name)
-    const obj = await parse(href)
+    const href = await run(name)
+    const parseData = await parse(href)
     spinner.stop();
 
-    const p = await print(obj)
+    const { version } = await prompt(prompts[1])
 
-    const version = await prompt({
-        type: 'text',
-        name: 'number',
-        message: 'version 을 선택해주세요'
-    })
-
-    const url = await getUrl(obj, version.number)
-    const type = await prompt({
-        type: 'text',
-        name: 'category',
-        message: 'maven(1) / gradle(2)'
-    })
-    spinner.start()
-    const r = await result(url, type.category)
-    spinner.stop()
+    const url = getSelectedUrl(parseData, version)
+    const { category } = await prompt(prompts[2])
+    result(url, category)
 }
